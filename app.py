044488e4ff5f2ab1d0689304b1d64f6bb1b4cf5a
Samuel Lavoie <lavoies360@gmail.com>
2023-09-18 09:34:15 -0400

    Header et Footer fait

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Objet:
+    def __init__(self, titre, description, photo, categorie):
+        self.titre = titre
+        self.description = description
+        self.photo = photo
+        self.categorie = categorie
+
 @app.route("/")
 def bonjour():
-    return render_template("modele.jinja", message="non")
-
+    return render_template("Main.jinja", pagePrincipale=True)
 
 if __name__ == "__main__":
     app.run()
