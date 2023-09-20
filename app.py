6f767e1e43e47f306c4176fbe745ef63ae161f71
Samuel Lavoie <lavoies360@gmail.com>
2023-09-20 11:55:55 -0400

    ajout templates

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import bd
 
 
 app = Flask(__name__)
@@ -12,7 +13,16 @@ class Objet:
 
 @app.route("/")
 def bonjour():
-    return render_template("Main.jinja", pagePrincipale=True)
+    objets = []
+
+    objets.append(Objet("Tigre", "Jésus Marie Joseph de batèche de sacristi de bout d'ciarge de saint-ciboire de sacrament de mosus de cul de viande à chien de charogne de colon de gériboire de boswell d'esprit de marde de câline de bine.", "templateImage.jpeg", 4))
+    objets.append(Objet("Deuxième Tigre", "Sapristi d'ostifie de cibouleau de sainte-viarge de batince de saint-cimonaque de saint-ciboire de tabarnouche.", "templateImage.jpeg", 5))
+    objets.append(Objet("Wow, un autre tigre", "Crucifix de tabarslaque de cibole de calvince de baptême de saint-sacrament de ciboire de charrue.", "templateImage.jpeg", 3))
+    objets.append(Objet("Deuxième Tigre", "Sapristi d'ostifie de cibouleau de sainte-viarge de batince de saint-cimonaque de saint-ciboire de tabarnouche.", "templateImage.jpeg", 5))
+
+    return render_template("base.jinja", titre="Accueil", objets=objets)
+
 
 if __name__ == "__main__":
+    bd.creer_connexion()
     app.run()
